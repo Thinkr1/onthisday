3d5f486fd327912190b27cdd7d0afeaa6ab1e823
Thinkr1 <146010473+Thinkr1@users.noreply.github.com>
2024-03-24 20:30:48 +0100

    Added necessary code from https://github.com/Thinkr1/On-This-Day project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
 
-function App() {
+const OnThisDay = () => {
+  const [day, setDay] = useState("");
+  const [month, setMonth] = useState("");
+  const [events, setEvents] = useState([]);
+
+  const handleSubmit = async () => {
+    const url = `https://api.wikimedia.org/feed/v1/wikipedia/en/onthisday/all/${month}/${day}`;
+    const headers = {
+      Authorization: `${process.env.REACT_APP_ACCESS_TOKEN}`,
+      "Api-User-Agent": "OnThisDay (a@a.com)",
+    };
+
+    try {
+      const response = await axios.get(url, { headers });
+      setEvents(response.data.events);
+    } catch (err) {
+      console.error(err);
+      setEvents(["Error: " + err.message]);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        value={day}
+        onChange={(e) => setDay(e.target.value)}
+        type="number"
+        placeholder="Day"
+      />
+      <input
+        value={month}
+        onChange={(e) => setMonth(e.target.value)}
+        type="number"
+        placeholder="Month"
+      />
+      <button onClick={handleSubmit}>Submit</button>
+      <div id="events">
+        {events.map((event, index) => (
+          <div key={index}>{event.text}</div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default OnThisDay;
